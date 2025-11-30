3b2c6d2e8c32dc6dbe737f0cf93a7ff6f34df31b
Shamiun <shamiunnabi360@gmail.com>
2025-11-30 13:48:11 +0600

    3rd change

--- src/main.c
+++ src/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int add(int a, int b)
 {
     return a + b;
 }
-
 int main()
 {
-    printf("Hello from C! 2 + 3 = %d\n", add(2, 3));
-    printf("Hello from C! 10 + 5 = %d\n", add(10, 5));
-    printf("Hello from C! 20 + 10 = %d\n", add(20, 10));
-    return 0;
+    printf("Summation of the numbers is 2 + 3 = %d\n", add(2, 3));
+    printf("Summation of the number is 10 + 5 = %d\n", add(10, 5));
+    printf("Summation of the number is 20 + 10 = %d\n", add(20, 10));
+
+return 0;
 }
