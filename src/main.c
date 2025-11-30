a8612b72eaf879031bca5871a308ba40296c618f
Shamiun <shamiunnabi360@gmail.com>
2025-11-30 13:34:05 +0600

    3rd changed

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int add(int a, int b)
 int main()
 {
     printf("Hello from C! 2 + 3 = %d\n", add(2, 3));
-     printf("Hello from C! 10 + 5 = %d\n", add(10, 5));
+    printf("Hello from C! 10 + 5 = %d\n", add(10, 5));
+    printf("Hello from C! 20 + 10 = %d\n", add(20, 10));
     return 0;
 }
